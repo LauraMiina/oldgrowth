837d6e7054fc8f91315fe8f34f28642395e3b3fa
LauraMiina <LauraMiina@users.noreply.github.com>
2024-08-09 11:14:52 -0400

    Update config.js

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ var config = {
             alignment: 'centre',
             hidden: false,
             title: 'Historic counting of the trees',
-            description: 'President Joe Biden´s administration issued an executive order in 2022 to “conserve America’s mature and old-growth forests on federal lands”. The map shows where all these old forests are, based on a recent inventory by the U.S. Forest Service. The darker the colors, the more old woods there are inside a fireshed, an area of 250,000 acres related to the USFS´s role as a fire-prevention agency.',
+            description: 'President Joe Biden´s administration issued an executive order in 2022 to “conserve America’s mature and old-growth forests on federal lands.” The map shows where all these old forests are, based on a recent inventory by the U.S. Forest Service. The darker the colors, the more old woods are inside a fireshed, an area of 250,000 acres related to the USFS´s role as a fire-prevention agency.',
             location: {
                 center: [-94.786879, 35.975403],
                 zoom: 4.2,
@@ -52,7 +52,7 @@ var config = {
             hidden: false,
             title: 'It used to be a billion acres',
             image: 'white-noheader-old-growth.png',
-            description: 'From the 193 million acres of federal forests, around 18 percent is deemed to be old-growth. That would mean 33 million acres, roughly three percent of the billion acres of forest that originally covered the country before the settlers arrived.',
+            description: 'Around 18 percent of the 193 million acres of federal forests are deemed to be old-growth. That would mean 33 million acres, roughly three percent of the billion acres of forest that originally covered the country before the settlers arrived.',
             location: {
                 center: [-103.771744, 38.585347],
                 zoom: 4.3,
@@ -91,7 +91,7 @@ var config = {
             hidden: false,
             title: 'West rules in old forest density',
          //   image: './path/to/image/source.png',
-            description: 'There is very little old-growth left in the east of the country. Largest areas with old-growth and mature forests are in the West. The highest concentration of forests that have never been cut is in Alaska.',
+            description: 'There is very little old-growth left in the east of the country. The largest areas with old-growth and mature forests are in the West. Alaska has the highest concentration of forests that have never been cut.',
             location: {
                 center: [-118.212542, 49.677534],
                 zoom: 4,
@@ -123,7 +123,7 @@ var config = {
             alignment: 'right',
             hidden: false,
             title: 'Fights for the forests',
-            description: 'There are over 20 controversial forest management projects that the Climate Forests coalition believe to be a threat to old-growth and mature forests. This list of projects compiled by the coalition of over 120 environmental organizations highlights ongoing disputes over the protection of the oldest trees across America.',
+            description: 'There are over 20 controversial forest management projects that the Climate Forests coalition believes to be a threat to old-growth and mature forests. This list of projects compiled by the coalition of over 120 environmental organizations highlights ongoing disputes over the protection of the oldest trees across America.',
         //    title: 'Proposed logging in the projects',
             image: 'orange_range.svg',
             location: {
@@ -158,7 +158,7 @@ var config = {
             hidden: false,
             title: 'The last old trees',
           //  image: './path/to/image/source.png',
-            description: 'In the Telephone Gap project the USFS is proposing forestry operations like logging, harvesting and thinning to approximately 12,000 acres of forest. Over 800 acres of this is old-growth, according to an environmental nonprofit Standing Trees. The USFS is saying that there will be no logging in old-growth areas within this project.',
+            description: 'In the Telephone Gap project, the USFS is proposing forestry operations like logging, harvesting, and thinning to approximately 12,000 acres of forest. Over 800 acres of this is old-growth, according to an environmental nonprofit, Standing Trees. The USFS says there will be no logging in old-growth areas within this project.',
             location: {
                 center: [-73.506591, 43.072033],
                 zoom: 7,
